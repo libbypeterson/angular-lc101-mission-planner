e80e05d45d3a3ef929711d72464d59c20de3809d
Libby Peterson <elibbypeterson@gmail.com>
2022-03-24 19:56:25 -0500

    made equipment editable

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: object[] = [ {name: "Habitat dome"}, {name: "Drones"}, {name: "Food containers"}, 
+  {name: "Oxygen tanks"}
+  ]
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(equipmentName: string) {
+    this.equipment.push({name: equipmentName})
+  }
+  
 }
