44b77172bceeba7520d817c4cbd996a4caec3639
Libby Peterson <elibbypeterson@gmail.com>
2022-03-29 22:47:10 -0500

    crew members can be selected and photos show

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -9,6 +9,7 @@ export class CrewComponent implements OnInit {
 
   inCrew: boolean = false;
   crew: object[] = [];
+  hoveredCandidate: object;
 
   candidates: object[] = [
     {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
@@ -30,11 +31,24 @@ for (let i = 0; i < this.crew.length; i++) {
     this.crew.splice(i, 1);
     return
   }
-};
+}
 if (this.crew.length >= 3) {
   return
 }
 this.crew.push(candidate);
+};
+
+isCandidateSelected(candidate: object): boolean {
+  for (let i = 0; i < this.crew.length; i++) {
+    if (this.crew[i]['name'] === candidate['name']) {
+      return true;
+    }
+}
+return false
+};
+
+isHovered(candidate: object) {
+  this.hoveredCandidate = candidate;
 };
 
 }
